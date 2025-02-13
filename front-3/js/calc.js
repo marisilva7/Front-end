cb432d3ba04ca9fc84cb864dde04a3b2632317c8
marisilva7 <Mariani.alves27@outlook.com>
2025-02-13 10:19:13 -0300

    adicinando novos arquivos js

---
+++ front-3/js/calc.js
@@ -0,0 +1,16 @@
+// declaração de variáveis
+let num1 = prompt("Digite um numero");
+let num2 = prompt("Digite outro numero");
+
+// operações matemáticas
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+
+// exibindo os resultados no console
+console.log("O resulatdo da soma é: " + soma);
+console.log("O resultado da sub é: " + sub);
+console.log("O resultado da mult é: " + mult);
+console.log("O resultado da div é: " + div);
