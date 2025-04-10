d9d1dbf54687960a17fe43ddc19ae486659698f4
marisilva7 <Mariani.alves27@outlook.com>
2025-04-10 08:53:32 -0300

    adicionando informações

---
+++ front-9/script.js
@@ -0,0 +1,30 @@
+const cars = [
+    {
+        brand: "Toyota",
+        model: "Corolla",   
+        year: 2020
+    },
+    {
+        brand: "Honda",
+        model: "Civic",   
+        year: 2019
+    },
+    {
+        brand: "Ford",
+        model: "Fiesta",
+        year: 2014
+    },
+    {
+        brand: "Ford",
+        model: "Focus",   
+        year: 2018
+    }
+];
+
+// criando uma funcao
+
+// console.log(cars[1])  selecionar apenas o segundo elemento "Honda"
+
+
+console.log(cars[0].brand); // selecionar apenas o primeiro elemento "Toyota"
+console.log(cars[2].model); // selecionar apenas o segundo elemento "Fiesta"
